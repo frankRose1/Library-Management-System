a3a7c5df3aa11ddb309b715401af30b932d564e3
Frank Rosendorf <frank.rosendorf1@gmail.com>
2019-01-30 14:29:35 -0500

    Add some more async-await and began refactoring routes

--- handlers/bookHandlers.js
+++ handlers/bookHandlers.js
@@ -3,116 +3,108 @@ const Loans = require('../models').Loans;
 const Patrons = require('../models').Patrons;
 const Sequelize = require('sequelize');
 const { Op } = Sequelize;
-//for pagination
+const createError = require('../utils/createError');
 const limit = 5; //show this amount of books per page
 
 
 const bookHandlers = {};
 
-bookHandlers.allBooks = (req, res, next) => {
-    const page = req.params.page || 1;
-    const offset = (page * limit) - limit;
+bookHandlers.allBooks = async (req, res) => {
+  const page = req.params.page || 1;
+  const offset = (page * limit) - limit;
 
-    //skip by the amount of the offset value and fetch the value of limit after that 
-    Books.findAndCountAll({
-        offset: offset,
-        limit: limit
-    }).then(books => {
-        //get book count and number of pages
-        const count = books.count;
-        const pages = Math.ceil(count / limit);
-        //if the user tries to access a page that returns no results, redirect them to the final page
-        if (!books.rows.length && offset) {
-            res.redirect(`/books/all/page/${pages}`);
-            return;
-        }
+  //skip by the amount of the offset value and fetch the value of limit after that 
+  const books = await Books.findAndCountAll({
+      offset: offset,
+      limit: limit
+  });
 
-        res.render('allBooks', {
-                                title: 'All Books', 
-                                books: books.rows, 
-                                page, 
-                                pages, 
-                                count
-                            });
-    }).catch(err => {
-        next(err);
-    });
+  const count = books.count;
+  const pages = Math.ceil(count / limit);
+  //if the user tries to access a page that returns no results, redirect them to the final page
+  if (!books.rows.length && offset) {
+    return res.redirect(`/books/all/page/${pages}`);
+  }
+
+  res.render('allBooks', {
+    title: 'All Books', 
+    books: books.rows, 
+    page, 
+    pages, 
+    count
+  });
 }
 
 bookHandlers.newBookForm = (req, res) => {
     res.render('newBookForm', { title: 'New Book', book: {} });
 };
 
-bookHandlers.filterBooks = (req, res, next) => {
-    const {query} = req.params;
-    const page = req.params.page || 1;
-    const offset = (page * limit) - limit;
-    //only books that are checked out && overdue
-    if (query == 'overdue') {
-        //SELECT * FROM books WHERE LOANS return_by < Date.now() && returned_on IS NULL
-        Books.findAndCountAll({
-            offset: offset,
-            limit: limit,
-            include : [
-                {
-                    model: Loans,
-                    where: {
-                        return_by : {
-                            [Op.lt]: Date.now()
-                        },
-                        returned_on: {
-                            [Op.eq]: null
-                        }
-                    }
-                }]
-        }).then(books => {
-            const count = books.count;
-            const pages = Math.ceil(count / limit);
-            res.render('filteredBooks', {
-                                        title: 'Overdue Books', 
-                                        books: books.rows,
-                                        page,
-                                        pages,
-                                        count,
-                                        query
-                                });
-        }).catch(err => {
-            return next(err);
-        });
-    }
-    //all books that are currently checked out
-    if (query == 'checked') {
-        // SELECT * FROM books WHERE LOANS returned_on IS NULL && loaned_on < Date.now()
-        Books.findAndCountAll({
-            offset: offset,
-            limit: limit,
-            include: [
-                {
-                    model: Loans,
-                    where: {
-                        returned_on: {
-                            [Op.eq]: null
-                        },
-                        loaned_on: {
-                            [Op.lt]: Date.now()
-                        }
-                    }
-                }]
-        }).then(books => {
-            const count = books.count;
-            const pages = Math.ceil(count / limit);
-            res.render('filteredBooks', {
-                                    title: 'Checked Out Books', 
-                                    books: books.rows,
-                                    page,
-                                    pages,
-                                    count,
-                                    query
-                                });
-        }).catch(err => {
-            next(err);
-        });
-    }
+bookHandlers.filterBooks = async (req, res) => {
+  const {query} = req.params;
+  const page = req.params.page || 1;
+  const offset = (page * limit) - limit;
+  let count;
+  let pages;
+  //only books that are checked out && overdue
+  if (query == 'overdue') {
+    //SELECT * FROM books WHERE LOANS return_by < Date.now() && returned_on IS NULL
+    const books = await Books.findAndCountAll({
+      offset: offset,
+      limit: limit,
+      include : [
+        {
+          model: Loans,
+          where: {
+            return_by : {
+              [Op.lt]: Date.now()
+            },
+            returned_on: {
+              [Op.eq]: null
+            }
+          }
+        }]
+    });
+    count = books.count;
+    pages = Math.ceil(count / limit);
+    res.render('filteredBooks', {
+      title: 'Overdue Books', 
+      books: books.rows,
+      page,
+      pages,
+      count,
+      query
+    });
+  }
+  //all books that are currently checked out
+  if (query == 'checked') {
+    // SELECT * FROM books WHERE LOANS returned_on IS NULL && loaned_on < Date.now()
+    const books = await Books.findAndCountAll({
+      offset: offset,
+      limit: limit,
+      include: [
+        {
+          model: Loans,
+          where: {
+            returned_on: {
+              [Op.eq]: null
+            },
+            loaned_on: {
+              [Op.lt]: Date.now()
+            }
+          }
+        }]
+    });
+    count = books.count;
+    pages = Math.ceil(count / limit);
+    res.render('filteredBooks', {
+      title: 'Checked Out Books', 
+      books: books.rows,
+      page,
+      pages,
+      count,
+      query
+    });
+  }
 };
 
 bookHandlers.addNewBook = (req, res, next) => {
@@ -137,32 +129,26 @@ bookHandlers.addNewBook = (req, res, next) => {
 };
 
 
-bookHandlers.getBookDetails = (req, res, next) => {
-    const bookId = req.params.id;
-    Books.findOne({
-        where: {
-            id: bookId
-        },
-        include: [
-            {
-                model: Loans,
-                include: [{
-                        model: Patrons,
-                        attributes: ['first_name', 'last_name', 'id']
-                    }
-                ]
-            }]
-    }).then(book => {
-        if (book) {
-            res.render('updateBookForm', {title: 'Book Details', book});
-        } else {
-            const error = new Error('Book not found');
-            error.status = 404;
-            return next(error);
-        }
-    }).catch(err => {
-        next(err);
-    });;
+bookHandlers.getBookDetails = async (req, res) => {
+  const bookId = req.params.id;
+  const book = await Books.findOne({
+    where: {
+        id: bookId
+    },
+    include: [
+      {
+        model: Loans,
+        include: [{
+            model: Patrons,
+            attributes: ['first_name', 'last_name', 'id']
+          }
+        ]
+      }]
+  });
+  if (!book){
+    createError('Book not found.', 404)
+  }
+  res.render('updateBookForm', {title: 'Book Details', book});
 };
 
 
@@ -212,28 +198,25 @@ bookHandlers.updateBook = (req, res, next) => {
 };
 
 //Users can search a book by title or author. search is case insensitive
-bookHandlers.searchBooks = (req, res, next) => {
-    const {search_query} = req.body;
-    Books.findAll({
-        where: {
-            [Op.or] : [
-                {
-                    title: {
-                        [Op.like] : `%${search_query}%`
-                    }
-                },
-                { 
-                    author: {
-                        [Op.like]: `%${search_query}%`
-                    }
-                }
-            ]
+bookHandlers.searchBooks = async (req, res) => {
+  const { search_query } = req.body;
+  const books = await Books.findAll({
+    where: {
+      [Op.or] : [
+        {
+          title: {
+            [Op.like] : `%${search_query}%`
+          }
+        },
+        { 
+          author: {
+            [Op.like]: `%${search_query}%`
+          }
         }
-    }).then(books => {
-        res.render('allBooks', {title: 'Books', books, search_query});
-    }).catch(err => {
-        next(err);
-    });
+      ]
+    }
+  });
+  res.render('allBooks', {title: 'Books', books, search_query});
 };
 
 module.exports = bookHandlers;
---
+++ routes/loans.js
@@ -0,0 +1,195 @@
+const express = require('express');
+const router = express.Router();
+const moment = require('moment');
+const Loans = require('../models').Loans;
+const Books = require('../models').Books;
+const Patrons = require('../models').Patrons;
+const Sequelize = require('sequelize');
+const { Op } = Sequelize;
+const createError = require('../utils/createError')
+const checkQueryParam = require('../middleware/checkQueryParam')
+
+/**
+ * All routes are prefixed with "/loans"
+ */
+
+router.get('/all', async (req, res) => {
+  const loans = await Loans.findAll({
+  include: [
+    {
+        model: Patrons
+    },
+    {
+        model: Books
+    }]
+  });
+  res.render('loansListing', {title: 'Loans', loans});
+});
+
+router.get('/filter/:query', checkQueryParam, async (req, res) => {
+  const { query } = req.params;
+  let loans;
+  if (query == 'checked') {
+    //SELECT * FROM loan WHERE loaned_on <= Date.now() AND returned_on IS NULL
+    loans = await Loans.findAll({
+      include:[
+        {
+            model: Patrons
+        },
+        {
+            model: Books
+        }],
+      where: {
+        loaned_on: { 
+            [Op.lte]: Date.now()
+        },
+        returned_on: {
+            [Op.eq]: null
+        }
+      }
+    });
+    res.render('loansListing', {title: 'Checked Out Books', loans});
+  } 
+
+  if (query == 'overdue') {
+    //SELECT * FROM loan WHERE return_by < DATE.now() AND returned_on IS NULL
+    loans = await Loans.findAll({
+      include: [
+        {
+          model: Patrons
+        },
+        {
+          model: Books
+        }],
+      where: {
+        return_by: {
+          [Op.lt]: Date.now()
+        },
+        returned_on: {
+          [Op.eq]: null
+        }
+      }
+    });
+    res.render('loansListing', {title: 'Overdue Loans', loans});
+  }
+});
+
+router.get('/new', async (req, res) => {
+  //these are the auto populated fields for the loan form
+  const loaned_on = moment().format('YYYY-MM-DD');
+  const return_by = moment().add(1, 'week').format('YYYY-MM-DD');
+  const [books, patrons] = await Promise.all([Books.findAll(), Patrons.findAll()]);
+  res.render('newLoanForm', {
+    title: 'New Loan',
+    books,
+    patrons,
+    loaned_on, 
+    return_by
+  });
+});
+
+router.post('/new', (req, res, next) => {
+  const {loaned_on, return_by} = req.body;
+
+  Loans.create(req.body)
+      .then(loan => {
+          //if it saves succefully, redirect to all loans
+          res.redirect('/loans/all');
+      })
+      .catch(err => {
+          if (err.name == 'SequelizeValidationError') {
+              //re render the form with the errors
+              Books.findAll()
+                  .then(books => {
+                      Patrons.findAll()
+                          .then(patrons => {
+                              res.render('newLoanForm', {
+                                  title: 'New Loan',
+                                  errors: err.errors,
+                                  books,
+                                  patrons,
+                                  loaned_on,
+                                  return_by
+                              });
+                          }).catch(err => {
+                              return next(err);
+                          });
+                  }).catch(err => {
+                      return next(err);
+                  });
+          } else {
+              throw err;
+          }
+      })
+      .catch(err => {
+          //server error creating new loan
+          next(err);
+      });
+});
+
+router.get('/returns/:id', async (req, res) => {
+  const todaysDate = moment().format('YYYY-MM-DD');
+  const {id} = req.params;
+  const loan = await Loans.findOne({
+    where: {
+        id: id
+    },
+    include: [
+        {
+            model: Patrons
+        },
+        {
+            model: Books
+        }]
+  });
+
+  if (!loan){
+    createError('Loan not found.', 404)
+  }
+
+  res.render('returnBookForm', {title: 'Return Book', loan, todaysDate});
+});
+
+router.post('/returns/:id', (req, res, next) => {
+  const {id} = req.params;
+  const {returned_on} = req.body;
+  Loans.findById(id)
+      .then(loan => loan.update({returned_on}))
+      .then(loan => {
+          //on a succesfull update, redirect to the loans page
+          res.redirect('/loans/all');
+      }).catch(err => {
+          if (err.name == 'SequelizeValidationError') {
+              //get the loan and associated patron/book
+              Loans.findOne({
+                  where: {
+                      id: id
+                  },
+                  include: [
+                      {
+                          model: Patrons
+                      },
+                      {
+                          model: Books
+                  }]
+              }).then(loan => {
+                  res.render('returnBookForm', {
+                      title: 'Return Books',
+                      todaysDate: returned_on,
+                      errors: err.errors,
+                      loan
+                  });
+              }).catch(err => {
+                  const error = new Error('Loan not found.');
+                  error.status = 404;
+                  return next(error);
+              });
+          } else {
+              throw err;
+          }
+      }).catch(err => {
+          next(err);
+      });
+});
+
+module.exports = router
